a83fb27f94d6ae1fa398ff7e7ebcee9632601efd
Amit Nafshi <amit.nafshi42@gmail.com>
2023-05-28 10:15:50 -0400

    edited bitset.cpp

--- projects/project1/bitset.cpp
+++ projects/project1/bitset.cpp
@@ -1,19 +1,139 @@
 #include "bitset.hpp"
 
-Bitset::Bitset() {
-    // TODO
+// Default constructor
+Bitset::Bitset()
+{
+    bitSize = 8;
+    bitData = new uint8_t[bitSize]{}; // {} at the end initializes all elements to 0
+    valid = true;
 }
 
-Bitset::Bitset(intmax_t size) {
-    // TODO
+// Constructor (size parameter)
+Bitset::Bitset(intmax_t size) 
+{
+    // Check and initialize bit size 
+    if (size <= 0)
+    {
+        valid = false;
+    }
+    else
+    {
+        bitSize = size;
+        valid = true;
+    }
+    bitData = new uint8_t[bitSize]{}; // {} at the end initializes all elements to 0
 }
 
-Bitset::Bitset(const std::string & value) {
-    // TODO
+// Constructor (string parameter)
+Bitset::Bitset(const std::string & value) 
+{
+    bitSize = value.length();
+    valid = true;
+    bitData = new uint8_t[bitSize];  // no {}, just set the array size
+
+    // Check if string is valid and fill data array bit by bit until error or end
+    for (intmax_t i = 0; i < bitSize; i++)
+    {
+        if (value[i] == '0')
+        {
+            bitData[i] = 0;
+        }
+        else if (value[i] == '1')
+        {
+            bitData[i] = 1;
+        }
+        else
+        {
+            valid = false;
+        }
+    }
 }
 
-Bitset::~Bitset() {
-    // TODO
+// Destructor
+Bitset::~Bitset() 
+{
+    delete [] bitData;
 }
 
-// TODO: other methods
+// Return bitset size
+intmax_t Bitset::size() const
+{
+    return bitSize;
+}
+
+// Determine if bitset is valid
+bool Bitset::good() const
+{
+    return valid;
+}
+
+// Set nth bit to 1
+void Bitset::set(intmax_t index)
+{
+    if (index >= 0 && index < bitSize)
+    {
+        bitData[index] = 1;
+    }
+    else
+    {
+        valid = false;
+    }
+}
+
+// Set nth bit to 0
+void Bitset::reset(intmax_t index)
+{
+    if (index >= 0 && index < bitSize)
+    {
+        bitData[index] = 0;
+    }
+    else
+    {
+        valid = false;
+    }
+}
+
+// Set Toggle nth bit
+void Bitset::toggle(intmax_t index)
+{
+    if (index < 0 || index >= bitSize)
+    {
+        valid = false;
+    }
+    else
+    {
+        if (bitData[index] == 0)
+        {
+            bitData[index] = 1;
+        }
+        else if (bitData[index] == 1)
+        {
+            bitData[index] == 0;
+        }
+    }
+}
+
+// Set Toggle nth bit
+bool Bitset::test(intmax_t index)
+{
+    if (index >= 0 && index < bitSize)
+        {
+            return bitData[index] == 1;
+        }
+        else
+        {
+            valid = false;
+            return false;
+        }
+}
+
+// Return bitset as string
+std::string Bitset::asString() const
+{
+    std::string result;
+    for (intmax_t i = bitSize-1; i >= 0; i--)
+    {
+        result += bitData[i] == 0 ? '0' : '1'; // conditonal operator to check index and convert to string accordingly
+    }
+    return result;
+}
